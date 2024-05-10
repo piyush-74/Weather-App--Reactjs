079baddb6dfdfc9af3ba49fc3adcb3bebb89ab24
Piyush Chourasia <piyushchourasia6666@gmail.com>
2024-05-10 09:29:34 +0530

    first commit

---
+++ src/apiKeys.js
@@ -0,0 +1,9 @@
+
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+
+module.exports = {
+    key: '1fdad93f5e9886914fce141e6b33bb16',
+    base: "https://api.openweathermap.org/data/2.5/",
+  };
+
+  
